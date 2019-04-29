c52c8dfc3467e96abf7e9e320f76e01fe3a265bc
YS00570545 <YS00570545@INHYTZPC01450>
2019-04-29 14:56:46 +0530

    Add project files.

---
+++ RevenueForecast.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RevenueForecast.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ RevenueForecast.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RevenueForecast.API.WebApiApplication" Language="C#" %>
